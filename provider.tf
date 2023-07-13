b8842eac811341cf62ef501e1d39697fa6b31593
premthat93 <139450715+premthat93@users.noreply.github.com>
2023-07-13 14:33:11 +0500

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">=1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+# Define the Azure provider configuration
+provider "azurerm" {
+  features {}
+}
